ee4afc5fe78a675e024cdcc2653c23a8eb947df7
iSommie <anajembaedwin@gmail.com>
2022-12-17 19:02:39 +0100

    typescript file setup

---
+++ build/index.js
@@ -0,0 +1,4 @@
+"use strict";
+var MYNUM = 7;
+var MYSTRING = "Welcome to connect section";
+console.log("My number is ".concat(MYNUM, " and my greeting is ").concat(MYSTRING));
---
+++ src/index.ts
@@ -0,0 +1,4 @@
+const MYNUM: number = 7;
+const MYSTRING: string = "Welcome to connect section";
+
+console.log(`My number is ${MYNUM} and my greeting is ${MYSTRING}`);
