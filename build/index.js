9f5a104060a33bcb46f7c462d57cbb87fbd73ec4
Modebe Stephen <modebestephen2@gmail.com>
2022-12-17 19:16:14 +0100

    Server built with express

--- build/index.js
+++ build/index.js
@@ -1,4 +1,12 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var express_1 = __importDefault(require("express"));
 var MYNUM = 7;
 var MYSTRING = "Welcome to connect section";
 console.log("My number is ".concat(MYNUM, " and my greeting is ").concat(MYSTRING));
+var app = (0, express_1.default)();
+var port = 3000;
+app.listen(port, function () { console.log("listen on port ".concat(port)); });
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,11 @@
+import express from "express";
 const MYNUM: number = 7;
 const MYSTRING: string = "Welcome to connect section";
 
 console.log(`My number is ${MYNUM} and my greeting is ${MYSTRING}`);
+
+const app = express();
+const port = 3000;
+
+
+app.listen(port, ()=> {console.log(`listen on port ${port}`)});
